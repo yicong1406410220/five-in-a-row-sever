c80f256812cb8b18b37b9d16c053281bde5ee18d
yicong1406410220 <azazaz123456@foxmail.com>
2019-10-06 13:11:30 +0800

    添加项目文件。

---
+++ five-in-a-row-sever/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace five_in_a_row_sever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!DbManager.Connect("wuziqi", "127.0.0.1", 3306, "root", ""))
+            {
+                return;
+            }
+            NetManager.StartLoop(8888);
+        }
+    }
+}
