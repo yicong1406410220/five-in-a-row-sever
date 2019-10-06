7d4b6df038d02b919134bc46a138786afffbba81
yicong1406410220 <azazaz123456@foxmail.com>
2019-10-06 14:28:26 +0800

    第2次提交

--- five-in-a-row-sever/script/logic/PlayerData.cs
+++ five-in-a-row-sever/script/logic/PlayerData.cs
@@ -1,6 +1,24 @@
 ﻿public class PlayerData{
 	//金币
 	public int coin = 0;
-	//记事本
-	public string text = "new text";
+    //等级
+    public int level = 0;
+    //经验
+    public int EXP = 0;
+    //段位
+    public int DuanWei = 0;
+    //段位经验
+    public int DuanWeiEXP = 0;
+    //vip等级
+    public int VIPLevel = 0;
+    //vip等级
+    public int VIPEXP = 0;
+    //胜利数
+    public int WinNumber = 0;
+    //失败数
+    public int LoseNumber = 0;
+    //失败数
+    public int GameTieNumber = 0;
+    //逃跑数
+    public int FlightNumber = 0;
 }
